3d13a0031c89f310a421fbba7bef15e41bc081ad
paul <paullopez8578@gmail.com>
2024-06-27 13:54:00 -0500

    El primer paso

---
+++ src/test/java/com/idat/semana5/Semana5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.semana5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
